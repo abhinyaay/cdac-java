c737bbce94bd220b271e0a1c0c7b6602af238a11
abhinyaay <abhinyaay@gmail.com>
2026-04-20 01:49:12 +0530

    add new folder

---
+++ java-tutorial/menu.java
@@ -0,0 +1,11 @@
+public class menu{
+    public static void main(String[] args){
+        System.out.println( "check");
+    }
+}
+
+// public class hello {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//     }
+// }
